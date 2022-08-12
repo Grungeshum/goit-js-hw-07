ee5b5d11f147987ce85591afd7a8542a4ab4d9c9
Igor Mazur <62945459+Grungeshum@users.noreply.github.com>
2022-08-12 11:54:42 +0300

    adds task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+  const galleryItem = document.createElement('div');
+  galleryItem.className = 'gallery__item';
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute('data-source', element.original);
+  galleryImage.alt = element.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+gallery.addEventListener('click', evt => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const selectedImage = evt.target.getAttribute('data-source');
+  const instance = basicLightbox.create(`<img src="${selectedImage}" width="800" heigth="600">`);
+
+  instance.show();
+
+  // close modal esc
+
+  gallery.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+});
